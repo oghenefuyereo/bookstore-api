d98e693539c28b3c725cee0411048fd74809e64e
oghenefuyereo <oghenefuyereo@gmail.com>
2025-08-02 12:46:37 -0400

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,19 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 const validateUser = require('../middleware/validateUser');
 
+// Get all users
 router.get('/', userController.getUsers);
+
+// Get user by ID  
+router.get('/:id', userController.getUserById);
+
+// Create a new user (with validation)
 router.post('/', validateUser, userController.createUser);
-router.put('/:id', userController.updateUser);
+
+// Update user by ID (you should add validation here too if desired)
+router.put('/:id', validateUser, userController.updateUser);
+
+// Delete user by ID
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
