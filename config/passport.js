bcb54fddea567fa68f0d7b0284817df412a8103f
oghenefuyereo <oghenefuyereo@gmail.com>
2025-08-02 11:18:53 -0400

    update

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/user');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
+  callbackURL: process.env.GOOGLE_CALLBACK_URL // ✅ use full callback URL
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const existingUser = await User.findOne({ googleId: profile.id });
@@ -24,6 +24,9 @@ passport.use(new GoogleStrategy({
 }));
 
 passport.serializeUser((user, done) => done(null, user.id));
+
 passport.deserializeUser((id, done) => {
-  User.findById(id).then(user => done(null, user)).catch(err => done(err, null));
+  User.findById(id)
+    .then(user => done(null, user))
+    .catch(err => done(err, null));
 });
